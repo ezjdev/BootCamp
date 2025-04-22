f4759c3fef2002e7ee762f85fb821c0e9f68b77b
ezapekin <ezapekin@colvir.com>
2025-04-22 08:36:00 +0300

    Homework 4: Create a second homework assignment in the form of a Spring Boot application

--- src/main/java/com/colvir/bootcamp/homework2/AppRunner.java
+++ src/main/java/com/colvir/bootcamp/homework2/AppRunner.java
@@ -1,23 +1,27 @@
 package com.colvir.bootcamp.homework2;
 
 import com.colvir.bootcamp.homework2.api.TemperatureUnit;
-import com.colvir.bootcamp.homework2.config.ApplicationConfig;
 import com.colvir.bootcamp.homework2.service.ConverterService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.stream.Stream;
 
 @Slf4j
+@SpringBootApplication
 public class AppRunner {
 
     public static void main(String[] args) {
-        try (ConfigurableApplicationContext annotationConfigApplicationContext =
-                     new AnnotationConfigApplicationContext(ApplicationConfig.class)) {
-            ConverterService converterService = annotationConfigApplicationContext.getBean(ConverterService.class);
+        try (ConfigurableApplicationContext context
+                     = SpringApplication.run(AppRunner.class, args)){
+            ConverterService converterService = context.getBean(ConverterService.class);
             Stream.of(TemperatureUnit.values())
-                    .forEach(unit -> log.info("{} {}", unit, converterService.converter(unit.getName(), 20d)));
+                    .forEach(unit ->
+                            log.info("{} {}"
+                                    , unit
+                                    , converterService.converter(unit.getName(), 20d)));
         }
     }
 }
