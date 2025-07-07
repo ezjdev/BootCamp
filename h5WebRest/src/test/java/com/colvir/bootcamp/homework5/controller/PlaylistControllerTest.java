0681cb30a95da4b897a95800cf2fbf1f1c9aaf2b
ezapekin <ezapekin@colvir.com>
2025-07-07 10:03:17 +0300

    Homework 25: Added Distributed tracing and Spring Cloud Security

--- h5WebRest/src/test/java/com/colvir/bootcamp/homework5/controller/PlaylistControllerTest.java
+++ h5WebRest/src/test/java/com/colvir/bootcamp/homework5/controller/PlaylistControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,7 +32,6 @@ class PlaylistControllerTest {
     private PlaylistService playlistService;
 
     @Test
-    @WithMockUser(username = "chupacabra", authorities = {"ROLE_CHUPACABRA"})
     void getAllPlaylists() throws Exception {
 
         when(playlistService.getById(SONG_ID))
